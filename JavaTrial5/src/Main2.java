f177ab4e4625f16cc356abde7b2ce7d432a6968e
Alparslan <alparslanaydgn02@gmail.com>
2023-02-27 17:48:43 +0300

    Added isItPrime

--- JavaTrial5/src/Main2.java
+++ JavaTrial5/src/Main2.java
@@ -2,8 +2,20 @@
 public class Main2 {
 
 	public static void main(String[] args) {
-		String experiment = "experiment1";
-		System.out.println(experiment);
+		int number = 2;
+		boolean isItPrime = true;
+
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				isItPrime = false;
+			}
+		}
+
+		if (isItPrime == true) {
+			System.out.println(number + " is a Prime number");
+		} else {
+			System.out.println(number + " is a not Prime number");
+		}
 
 	}
 
