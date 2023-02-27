107bfab5fc62584aa9f56eee03e0a44cb0493c86
Alparslan <alparslanaydgn02@gmail.com>
2023-02-27 16:36:29 +0300

    Foreach code added

---
+++ JavaTrial5/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA!!!");
+		
+		String[] credits= {"car credit","mortgage credit","null credit"};
+		
+		for (String credit : credits) {
+			System.out.println(credit);
+		}
+		
+	}
+
+}
