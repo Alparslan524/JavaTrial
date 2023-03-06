21694724618cf196b7a1d30f64812b6df5524f9f
Alparslan <alparslanaydgn02@gmail.com>
2023-03-06 17:44:14 +0300

    Added business rules(Product name already exist)

--- oopWithNLayerApp/src/oopWithNLayerApp/Main.java
+++ oopWithNLayerApp/src/oopWithNLayerApp/Main.java
@@ -14,13 +14,15 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		Product product1 = new Product(1, "Poco M3", 5000);
-
+		Product product2 = new Product(2, "Poco M3", 500);
+		
 		// Logger[] loggers = { new MailLogger(), new DatabaseLogger() };
 		Logger[] loggers = { new MailLogger(), new DatabaseLogger(), new FileLogger() };
 		// İstediğin kadar log türü gönder
 
-		ProductManager productManager = new ProductManager(new HibernateProductDao(), loggers);
+		ProductManager productManager = new ProductManager(new JdbcProductDao(), loggers);
 		productManager.add(product1);
+		//productManager.add(product2);//Product name already exist
 
 	}
 
--- oopWithNLayerApp/src/oopWithNLayerApp/business/ProductManager.java
+++ oopWithNLayerApp/src/oopWithNLayerApp/business/ProductManager.java
@@ -1,5 +1,6 @@
 package oopWithNLayerApp.business;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import oopWithNLayerApp.core.logging.Logger;
@@ -10,6 +11,7 @@ public class ProductManager {
 
 	private ProductDao productDao;
 	private Logger[] loggers;
+	private List<Product> products= new ArrayList<>();
 	
 	public ProductManager(ProductDao productDao, Logger[] loggers) {
 		this.productDao = productDao;
@@ -17,11 +19,17 @@ public class ProductManager {
 	}
 
 	public void add(Product product) throws Exception {
-
+		
+		for (Product productList : products) {
+			if (productList.getName()==product.getName()) {
+				throw new Exception("Product name already exist " + product.getName());
+			}
+		}
+		
 		if (product.getUnitPrice() < 10) {
 			throw new Exception("Product price cannot be a less 10");
 		}
-		
+		products.add(product);
 		productDao.add(product);
 		
 		for (Logger logger : loggers) {
